045e4902521029d5eececaa3f47e25fdeb37d452
Tadas <61449927+tadasgo@users.noreply.github.com>
2020-08-04 12:31:54 +0300

    Completed modal feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,14 @@ import '../styles/styles.css';
 import Hamburger from './modules/Hamburger';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 // accept updates on the fly (hot module replacements)
 // accept if it makes sense to accept...
 if (module.hot) module.hot.accept();
 
-new Hamburger();
+const hamburger = new Hamburger();
+const stickyHeader = new StickyHeader();
 new RevealOnScroll('.feature-item', 80);
 new RevealOnScroll('.testimonial', 70);
-new StickyHeader();
+new Modal();
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,65 @@
+class Modal {
+	constructor() {
+		this.injectHTML();
+		this.modal = document.querySelector('.modal');
+		this.openModalBtns = document.querySelectorAll('.open-modal');
+		this.closeIcon = document.querySelector('.modal__close');
+		this.events();
+	}
+
+	events() {
+		// listen for openClick
+		this.openModalBtns.forEach((el) => el.addEventListener('click', (e) => this.openTheModal(e)));
+
+		// listen for closeClick
+		this.closeIcon.addEventListener('click', () => this.closeTheModal());
+
+		// listen for anyKey
+		document.addEventListener('keyup', (e) => this.keyPressHandler(e));
+	}
+
+	openTheModal(e) {
+		// prevent default of a links
+		e.preventDefault();
+		this.modal.classList.add('modal--visible');
+	}
+
+	closeTheModal() {
+		this.modal.classList.remove('modal--visible');
+	}
+
+	keyPressHandler(e) {
+		// escape keyCode = 27
+		if (e.keyCode === 27) {
+			this.closeTheModal();
+		}
+	}
+
+	injectHTML() {
+		document.body.insertAdjacentHTML(
+			'beforeend',
+			`
+      <div class="modal">
+        <div class="modal__inner">
+          <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg"
+              class="section-title__icon"> Get in <strong>Touch</strong></h2>
+          <div class="wrapper wrapper--narrow">
+            <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on
+              any of the platforms below!</p>
+          </div>
+
+          <div class="social-icons">
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
+          </div>
+        </div>
+        <div class="modal__close">&times;</div>
+      </div>
+      `
+		);
+	}
+}
+
+export default Modal;
