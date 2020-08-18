a7ca3ad3e80e273cc273f1ac77fe16ce8b694fc8
Tadas <61449927+tadasgo@users.noreply.github.com>
2020-08-18 23:16:20 +0300

    Testing out cloud functions

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,8 +1,25 @@
 exports.handler = (event, context, callback) => {
 	// within this function we are safe to perform backend functions, because they are handled by AWS. Public only see value we respond with
+	const secretContent = `
+		<h3>Welcome to the <strong>secret area</strong></h3>
+	`;
+	let body;
+	// if there is any data inside event parse data as JSON else create empty object
+	if (event.body) {
+		body = JSON.parse(event.body);
+	} else {
+		body = {};
+	}
 
-	callback(null, {
-		statusCode: 200,
-		body: 'Welcome to the secret area',
-	});
+	if (body.password === 'pass123') {
+		callback(null, {
+			statusCode: 200,
+			body: 'Welcome to the secret area',
+		});
+	} else {
+		callback(null, {
+			// Unauthorized
+			statusCode: 401,
+		});
+	}
 };
